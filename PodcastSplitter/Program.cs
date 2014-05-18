1e42be78deed01c0b53923ef6f2560caf215ab3d
Jim Barnett <james.barnett.jr@gmail.com>
2014-05-17 20:09:04 -0500

    Minor tweaks

--- PodcastSplitter/Program.cs
+++ PodcastSplitter/Program.cs
@@ -15,16 +15,16 @@ namespace PodcastSplitter
             var config = ConfigParser.Parse();
             string podcasts = Path.Combine(config["root"], config["podcast"]);
 
-            Debug.WriteLine(string.Format("mp3split_split_options: {0}", config["mp3split_split_options"]));
+            Debug.WriteLine(string.Format("mp3split_split_options: {0}", config["mp3splt_split_options"]));
 
             string options = string.Format("{0} -d {1} -o {2} {3}",
-                config["mp3split_split_options"],
+                config["mp3splt_split_options"],
                 config["outputdir"], 
-                config["mp3split_output_options"],
+                config["mp3splt_output_options"],
                 string.Format("\"{0}\"", podcasts));
             
             Process p = new Process();
-            p.StartInfo.FileName = config["mp3split"];
+            p.StartInfo.FileName = config["mp3splt"];
             p.StartInfo.Arguments = options;
             p.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
             p.Start();
